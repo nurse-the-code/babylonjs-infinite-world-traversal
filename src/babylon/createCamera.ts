5887b574f2c3cebf613fcdf0cdf10664e473b091
Malachi Gruenhagen <68450431+nurse-the-code@users.noreply.github.com>
2024-03-25 12:04:16 -0400

    Configured grid. Switched to UniversalCamera. Changed location of initial first-person camera perspective.

--- src/babylon/createCamera.ts
+++ src/babylon/createCamera.ts
@@ -1,18 +1,18 @@
-import { FreeCamera, Scene, TargetCamera, Vector3 } from "@babylonjs/core";
+import { FreeCamera, Scene, UniversalCamera, Vector3 } from "@babylonjs/core";
 
-const createCamera = (
-  scene: Scene,
-  canvas: HTMLCanvasElement,
-): TargetCamera => {
+// According to the infallible ChatGPT
+const AVERAGE_EYE_HEIGHT_METERS: number = 1.57;
+
+const createCamera = (scene: Scene, canvas: HTMLCanvasElement): FreeCamera => {
   // Create a position a free camera (non-mesh)
-  const camera: TargetCamera = new FreeCamera(
+  const camera: FreeCamera = new UniversalCamera(
     "camera1",
-    new Vector3(0, 5, -10),
+    new Vector3(0, AVERAGE_EYE_HEIGHT_METERS, 0),
     scene,
   );
 
-  // Make the camera look at the scene origin
-  camera.setTarget(Vector3.Zero());
+  // Make the camera look down at the ground about 20 meters away.
+  camera.setTarget(new Vector3(20, 0, 0));
 
   // Attach the camera to the canvas
   camera.attachControl(canvas, true);
--- src/babylon/createGrid.ts
+++ src/babylon/createGrid.ts
@@ -2,13 +2,20 @@ import { CreateGround, GroundMesh, Material, Scene } from "@babylonjs/core";
 import { GridMaterial } from "@babylonjs/materials/grid/gridMaterial";
 
 const createMaterial = (scene: Scene): Material => {
-  return new GridMaterial("grid", scene);
+  const material = new GridMaterial("grid", scene);
+
+  // Grid configuration (1 unit is scaled to 1 meter)
+  material.majorUnitFrequency = 16; // Major grid lines every 16 minor lines
+  material.minorUnitVisibility = 0.2; // Opacity of minor grid lines
+  material.gridRatio = 0.0625; // Minor lines per 1 unit, inspired by Minecraft
+
+  return material;
 };
 
 const createGrid = (scene: Scene): GroundMesh => {
   const grid: GroundMesh = CreateGround("ground1", {
-    height: 64,
-    width: 64,
+    height: 32,
+    width: 32,
     subdivisions: 2,
   });
   grid.material = createMaterial(scene);
