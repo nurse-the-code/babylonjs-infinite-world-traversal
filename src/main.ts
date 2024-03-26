5721961f5b7c6a8973af69572f6b7626054bd87f
Malachi Gruenhagen <68450431+nurse-the-code@users.noreply.github.com>
2024-03-26 09:36:07 -0400

    Added jest. Minor changes to src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { SimpleBabylonScene } from "./babylon";
 document.addEventListener("DOMContentLoaded", () => {
   const canvasComponent: HTMLCanvasElement = MainCanvas.create();
   document.body.appendChild(canvasComponent);
-  // Further initialization of your Babylon.js scene can happen here
-  const simpleScene = new SimpleBabylonScene(canvasComponent);
+
+  const simpleScene: SimpleBabylonScene = new SimpleBabylonScene(
+    canvasComponent,
+  );
+
   simpleScene.renderLoop();
 });
